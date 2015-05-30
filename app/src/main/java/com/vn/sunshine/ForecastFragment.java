412bdd6b8a4e2fa84dcef5acc60d4748f6365e8a
Vishnu Narayanan <vishnu@pepalo.com>
2015-05-30 23:51:00 +0530

    get location from shared prefs

--- app/src/main/java/com/vn/sunshine/ForecastFragment.java
+++ app/src/main/java/com/vn/sunshine/ForecastFragment.java
@@ -5,9 +5,11 @@ package com.vn.sunshine;
  */
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.text.format.Time;
 import android.util.Log;
@@ -69,7 +71,12 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id== R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("thodupuzha");
+
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
