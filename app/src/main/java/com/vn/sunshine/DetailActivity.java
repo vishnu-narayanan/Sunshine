5d2d4fb5cb361600d8eb8349ea94426e2e91f5ee
Vishnu <iamwishnu@gmail.com>
2015-09-16 17:13:50 +0530

    Add ShareActionProvider to detail activity

--- app/src/main/java/com/vn/sunshine/DetailActivity.java
+++ app/src/main/java/com/vn/sunshine/DetailActivity.java
@@ -2,7 +2,10 @@ package com.vn.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,25 +13,44 @@ import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
+    private ShareActionProvider mShareActionProvider;
+    private final String LOG_TAG = DetailActivity.class.getSimpleName();
+    private String weather;
+    private static final String SHARE_HASHTAG = " #SunshineApp";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         Intent i = getIntent();
-        String weather = i.getStringExtra("weather");
+        weather = i.getStringExtra("weather");
 
         ((TextView) findViewById(R.id.detail)).setText(weather);
 
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_detail, menu);
+
+        // Locate MenuItem with ShareActionProvider
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+
+        // Fetch and store ShareActionProvider
+        mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+        if (mShareActionProvider != null ) {
+            mShareActionProvider.setShareIntent(displayShareOptions());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
         return true;
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -44,6 +66,18 @@ public class DetailActivity extends ActionBarActivity {
             return true;
         }
 
+    
+
         return super.onOptionsItemSelected(item);
+    }
+
+    private Intent displayShareOptions() {
+
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, weather + SHARE_HASHTAG);
+        sendIntent.setType("text/plain");
+        return sendIntent;
     }
 }
