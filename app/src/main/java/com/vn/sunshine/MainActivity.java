6229c2fa3d5da35a51b029c5102b79bf50fecd85
Vishnu <iamwishnu@gmail.com>
2015-09-23 14:46:29 +0530

    add lab activity for simultenous multiple location rendering

--- app/src/main/java/com/vn/sunshine/MainActivity.java
+++ app/src/main/java/com/vn/sunshine/MainActivity.java
@@ -54,6 +54,12 @@ public class MainActivity extends ActionBarActivity {
             return  true;
         }
 
+        if (id == R.id.action_lab){
+            Intent intent = new Intent(this,LabActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
 
